db54b15f71b9b9d348b2a9308c4f45b9de00f8a6
Jorge Reina Triguero <242613119+jorrgee8@users.noreply.github.com>
2026-05-13 13:50:01 +0100

    js prueba 1

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ boton.addEventListener('click', () =>{
         localStorage.setItem("tema", "claro");
     }
 })
+
+/* Estudios */
+
+let botonEstudios = document.getElementById("agregar-estudio");
+let modal = document.getElementById("modal");
+let cerrarModal = document.getElementById("cerrar-modal");
+
+let agregarEstudio = document.getElementById("agregar");
+
+//info
+
+let estudioAgregado = document.getElementById("estudio-agregado")
+
+let lista = document.getElementById("estudios");
+
+
+botonEstudios.addEventListener('click', () =>{
+    modal.style.display = "flex";
+})
+
+cerrarModal.addEventListener('click', () =>{
+    modal.style.display = "none";
+})
+
+agregarEstudio.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+    let titulo = document.getElementById("titulo").value;
+    let contenido = document.getElementById("info").value;
+
+    let li = document.createElement('li');
+
+        li.innerHTML = `<strong>${titulo}</strong><br>${contenido}`;
+
+        lista.appendChild(li);
+
+        estudioAgregado.innerHTML += `El estudio <strong>${titulo}</strong> se ha agregado <br>`
+
+        document.getElementById("formulario-estudio").reset()
+
+})
